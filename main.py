200df8848353f813aa2d7b88a5bab8646e091e71
Ashhad <ashhadhanafi@gmail.com>
2022-03-22 00:55:39 -0400

    Improved Output Formatting

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ print("This program generates prime values based on the formula n^2 - n + 41. It
 print("Below, specify the value of n you would like the program to generate up to.")
 num_values = int(input("n = "))
 
-username = os.getlogin()
-
 
 def calculate():
     result = []
@@ -37,12 +35,21 @@ def is_prime(result):
 
 
 def print_all(calculated_values, prime_values):
-    print()
-    print("All Values")
-    print('{:7}'.format("VALUE #"), '{:11}'.format("CALCULATION"), '{:6}'.format("PRIME?"), sep="\t\t\t")
+    # print()
+    # print("All Values")
+    # print()
+    # print("n =", num_values)
+    # print(is_prime(calculate()).count(1), "/", num_values, " Prime (",
+    #       '{:.2f}'.format((is_prime(calculate()).count(1) / num_values) * 100), "%)", sep="")
+    # print("------------------------------------------")
+    # print('{:7}'.format("VALUE #"), '{:11}'.format("CALCULATION"), '{:6}'.format("PRIME?"), sep="\t\t\t")
 
-    export_all = open(f"C:\\Users\\{username}\\Desktop\\PG-{num_values}-All.txt", "w")
-    export_all.write("All Values\n")
+    export_all = open(f"C:\\Users\\{os.getlogin()}\\Desktop\\PG-{num_values}-All.txt", "w")
+    export_all.write("All Values\n\n")
+    export_all.write("n = " + str(num_values) + "\n")
+    export_all.write(str(is_prime(calculate()).count(1)) + "/" + str(num_values) + " Prime (" +
+                     str('{:.2f}'.format((is_prime(calculate()).count(1) / num_values) * 100)) + "%)" + "\n")
+    export_all.write("--------------------------------------------------------------\n")
     export_all.write(
         '{:7}'.format("VALUE #") + "\t\t\t" + '{:11}'.format("CALCULATION") + "\t\t\t" + '{:6}'.format("PRIME?") + "\n")
 
@@ -50,8 +57,8 @@ def print_all(calculated_values, prime_values):
 
     for i in range(num_values):
         count += 1
-        print('{:7}'.format(count), '{:11}'.format(calculate()[i]), '{:6}'.format(is_prime(calculate())[i]),
-              sep="\t\t\t")
+        # print('{:7}'.format(count), '{:11}'.format(calculate()[i]), '{:6}'.format(is_prime(calculate())[i]),
+        #       sep="\t\t\t")
         export_all.write('{:7}'.format(count) + "\t\t\t" + '{:11}'.format(calculate()[i]) + "\t\t\t" + '{:6}'.format(
             is_prime(calculate())[i]) + "\n")
 
@@ -59,12 +66,14 @@ def print_all(calculated_values, prime_values):
 
 
 def print_prime(calculated_values, prime_values):
-    print()
-    print("Only Prime Values")
-    print('{:7}'.format("VALUE #"), '{:11}'.format("CALCULATION"), '{:6}'.format("PRIME?"), sep="\t\t\t")
+    # print()
+    # print("Only Prime Values")
+    # print("------------------------------------------")
+    # print('{:7}'.format("VALUE #"), '{:11}'.format("CALCULATION"), '{:6}'.format("PRIME?"), sep="\t\t\t")
 
-    export_prime = open(f"C:\\Users\\{username}\\Desktop\\PG-{num_values}-Prime.txt", "w")
-    export_prime.write("Only Prime Values\n")
+    export_prime = open(f"C:\\Users\\{os.getlogin()}\\Desktop\\PG-{num_values}-Prime.txt", "w")
+    export_prime.write("Only Prime Values\n\n")
+    export_prime.write("--------------------------------------------------------------\n")
     export_prime.write(
         '{:7}'.format("VALUE #") + "\t\t\t" + '{:11}'.format("CALCULATION") + "\t\t\t" + '{:6}'.format("PRIME?") + "\n")
 
@@ -73,8 +82,8 @@ def print_prime(calculated_values, prime_values):
     for i in range(num_values):
         count += 1
         if is_prime(calculate())[i]:
-            print('{:7}'.format(count), '{:11}'.format(calculate()[i]), '{:6}'.format(is_prime(calculate())[i]),
-                  sep="\t\t\t")
+            # print('{:7}'.format(count), '{:11}'.format(calculate()[i]), '{:6}'.format(is_prime(calculate())[i]),
+            #       sep="\t\t\t")
             export_prime.write(
                 '{:7}'.format(count) + "\t\t\t" + '{:11}'.format(calculate()[i]) + "\t\t\t" + '{:6}'.format(
                     is_prime(calculate())[i]) + "\n")
@@ -83,12 +92,14 @@ def print_prime(calculated_values, prime_values):
 
 
 def print_non_prime(calculated_values, prime_values):
-    print()
-    print("Only Non-Prime Values")
-    print('{:7}'.format("VALUE #"), '{:11}'.format("CALCULATION"), '{:6}'.format("PRIME?"), sep="\t\t\t")
+    # print()
+    # print("Only Non-Prime Values")
+    # print("------------------------------------------")
+    # print('{:7}'.format("VALUE #"), '{:11}'.format("CALCULATION"), '{:6}'.format("PRIME?"), sep="\t\t\t")
 
-    export_non_prime = open(f"C:\\Users\\{username}\\Desktop\\PG-{num_values}-Non_Prime.txt", "w")
-    export_non_prime.write("Only Prime Values\n")
+    export_non_prime = open(f"C:\\Users\\{os.getlogin()}\\Desktop\\PG-{num_values}-Non_Prime.txt", "w")
+    export_non_prime.write("Only Prime Values\n\n")
+    export_non_prime.write("--------------------------------------------------------------\n")
     export_non_prime.write(
         '{:7}'.format("VALUE #") + "\t\t\t" + '{:11}'.format("CALCULATION") + "\t\t\t" + '{:6}'.format("PRIME?") + "\n")
 
@@ -97,8 +108,8 @@ def print_non_prime(calculated_values, prime_values):
     for i in range(num_values):
         count += 1
         if not is_prime(calculate())[i]:
-            print('{:7}'.format(count), '{:11}'.format(calculate()[i]), '{:6}'.format(is_prime(calculate())[i]),
-                  sep="\t\t\t")
+            # print('{:7}'.format(count), '{:11}'.format(calculate()[i]), '{:6}'.format(is_prime(calculate())[i]),
+            #       sep="\t\t\t")
             export_non_prime.write(
                 '{:7}'.format(count) + "\t\t\t" + '{:11}'.format(calculate()[i]) + "\t\t\t" + '{:6}'.format(
                     is_prime(calculate())[i]) + "\n")
